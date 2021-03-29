504bd61cd7730d5c2be34f6f58c9545799bda9de
dorinma <dorinma@post.bgu.ac.il>
2021-03-29 19:40:39 +0300

    tmp #24

--- src/Permissions.java
+++ src/Permissions.java
@@ -13,20 +13,31 @@ public class Permissions {
         DefinePurchaseFormat,
         DefineDiscountPolicy,
         DefineDiscountFormat,
-        CloseStore,
+        CloseStore, //only for store creator
         ReopenStore,
         GetBossesInfo
     }
 
-    private Map<User, User> appointments; //Appointed, appointee
+    //private Map<User, User> appointments; //Appointed, appointee
     private List<Operations> operations;
 
-    public boolean addPermissions(int currUserId, int editUserId, List<Integer> operations) {
+    private Map<Integer, Integer> appointments; //Appointed, appointee (ids)
+    private Map<User, List<Operations>> usersPermissions;
+
+    public boolean addPermissions(int currUserId, int editUserId, List<Integer> opIndexes) {
+        for (User u: usersPermissions.keySet()) {
+
+
+        }
+        Operations[] ops = Operations.values();
+        for (int i: opIndexes) {
+            if(!this.operations.contains(ops[i-1]))
+                this.operations.add(ops[i-1]);
+        }
         return true;
     }
 
-    public boolean removePermissions(int currUserId, int editUserId, List<Integer> operations) {
-        return true;
+    public void removePermissions(int currUserId, int editUserId, List<Integer> opIndexes) {
     }
 
 }

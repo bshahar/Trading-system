fb3c92a8ade9eeb0a4ca3db69e0c607c230c7704
dorinma <dorinma@post.bgu.ac.il>
2021-03-29 17:08:27 +0300

    Start work on permission class #24

--- src/Permissions.java
+++ src/Permissions.java
@@ -1,5 +1,32 @@
 import java.util.*;
 
 public class Permissions {
+    enum Operations {
+        AddProduct,
+        RemoveProduct,
+        EditProduct,
+        AppointManager,
+        RemoveManagerAppointment,
+        AppointOwner,
+        RemoveOwnerAppointment,
+        DefinePurchasePolicy,
+        DefinePurchaseFormat,
+        DefineDiscountPolicy,
+        DefineDiscountFormat,
+        CloseStore,
+        ReopenStore,
+        GetBossesInfo
+    }
+
+    private Map<User, User> appointments; //Appointed, appointee
+    private List<Operations> operations;
+
+    public boolean addPermissions(int currUserId, int editUserId, List<Integer> operations) {
+        return true;
+    }
+
+    public boolean removePermissions(int currUserId, int editUserId, List<Integer> operations) {
+        return true;
+    }
 
 }

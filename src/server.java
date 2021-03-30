46e1716972b88e04874dbe9b4222fa177089e322
kandabior <kandabior@gmail.com>
2021-03-30 16:23:24 +0300

    tests #26

--- src/server.java
+++ src/server.java
@@ -1,84 +1,83 @@
-
-import java.net.*;
-import java.io.*;
-
-class Server {
-
-    private ServerSocket serverSocket;
-    private static TradingSystem tradingSystem;
-
-    public void start(int port) {
-        tradingSystem = new TradingSystem(null);
-        try {
-            System.out.println("server started");
-            serverSocket = new ServerSocket(port);
-            while (true)
-                new ClientHandler(serverSocket.accept()).start();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            stop();
-        }
-
-    }
-
-    public void stop() {
-        try {
-
-            serverSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private static class ClientHandler extends Thread {
-        private Socket clientSocket;
-        private PrintWriter out;
-        private BufferedReader in;
-
-        public ClientHandler(Socket socket) {
-            this.clientSocket = socket;
-        }
-
-        public void run() {
-            try {
-                out = new PrintWriter(clientSocket.getOutputStream(), true);
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                String inputLine;
-                while ((inputLine = in.readLine()) != null) {
-                    String action = inputLine.substring(0, inputLine.indexOf(' '));
-                    switch (action)
-                    {
-                        case "LOGIN":
-                            tradingSystem.login(inputLine);
-                            break;
-                        case "REGISTER":
-                            tradingSystem.register(inputLine);
-                            break;
-                    }
-                    if (".".equals(inputLine)) {
-                        out.println("bye");
-                        break;
-                    }
-                    System.out.println(inputLine);
-                    out.println(inputLine);
-                }
-
-                in.close();
-                out.close();
-                clientSocket.close();
-
-            } catch (IOException e) {
-
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        Server server = new Server();
-        server.start(5555);
-    }
-
-}
+//import java.net.*;
+//import java.io.*;
+//
+//class Server {
+//
+//    private ServerSocket serverSocket;
+//    private static TradingSystem tradingSystem;
+//
+//    public void start(int port) {
+//        tradingSystem = new TradingSystem();
+//        try {
+//            System.out.println("server started");
+//            serverSocket = new ServerSocket(port);
+//            while (true)
+//                new ClientHandler(serverSocket.accept()).start();
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            stop();
+//        }
+//
+//    }
+//
+//    public void stop() {
+//        try {
+//
+//            serverSocket.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//
+//    private static class ClientHandler extends Thread {
+//        private Socket clientSocket;
+//        private PrintWriter out;
+//        private BufferedReader in;
+//
+//        public ClientHandler(Socket socket) {
+//            this.clientSocket = socket;
+//        }
+//
+//        public void run() {
+//            try {
+//                out = new PrintWriter(clientSocket.getOutputStream(), true);
+//                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//                String inputLine;
+//                while ((inputLine = in.readLine()) != null) {
+//                    String action = inputLine.substring(0, inputLine.indexOf(' '));
+//                    switch (action)
+//                    {
+//                        case "LOGIN":
+//                            tradingSystem.login(inputLine);
+//                            break;
+//                        case "REGISTER":
+//                            tradingSystem.register(inputLine);
+//                            break;
+//                    }
+//                    if (".".equals(inputLine)) {
+//                        out.println("bye");
+//                        break;
+//                    }
+//                    System.out.println(inputLine);
+//                    out.println(inputLine);
+//                }
+//
+//                in.close();
+//                out.close();
+//                clientSocket.close();
+//
+//            } catch (IOException e) {
+//
+//            }
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        Server server = new Server();
+//        server.start(5555);
+//    }
+//
+//}

b703d44cd7c45722934592dd6bb49b8fe8d633b0
kandabior <kandabior@gmail.com>
2021-05-15 16:22:43 +0300

    add externals

--- src/Server/myPurchases.java
+++ src/Server/myPurchases.java
@@ -30,47 +30,57 @@ public class myPurchases {
     public void message(Session session, String message) throws IOException {
         JSONObject jo = new JSONObject(message);
         String type = jo.get("type").toString();
-        if(type.equals("GET_PURCHASES")){
-            int userId=Integer.parseInt(jo.get("userId").toString());
-            Result result= API.getUserPurchaseHistory(userId);
-            JSONObject jsonOut=new JSONObject();
-            jsonOut.put("type","GET_PURCHASES");
-            if(result.isResult()){
-                List<Receipt> receipts=(List<Receipt>)result.getData();
-                JSONObject[] receiptsJson=new JSONObject[receipts.size()];
-                int j=0;
-                for(Receipt receipt : receipts){
-                    JSONObject[] linesJson= new JSONObject[receipt.getLines().size()];
-                    int i=0;
-                    for(Receipt.ReceiptLine receiptLine : receipt.getLines()){
-                        JSONObject receiptLineJson= new JSONObject();
-                        receiptLineJson.put("prodName",receiptLine.getProdName());
-                        receiptLineJson.put("price",receiptLine.getPrice());
-                        receiptLineJson.put("amount",receiptLine.getAmount());
-                        linesJson[i]=receiptLineJson;
+        if (type.equals("GET_PURCHASES")) {
+            int userId = Integer.parseInt(jo.get("userId").toString());
+            Result result = API.getUserPurchaseHistory(userId);
+            JSONObject jsonOut = new JSONObject();
+            jsonOut.put("type", "GET_PURCHASES");
+            if (result.isResult()) {
+                List<Receipt> receipts = (List<Receipt>) result.getData();
+                JSONObject[] receiptsJson = new JSONObject[receipts.size()];
+                int j = 0;
+                for (Receipt receipt : receipts) {
+                    JSONObject[] linesJson = new JSONObject[receipt.getLines().size()];
+                    int i = 0;
+                    for (Receipt.ReceiptLine receiptLine : receipt.getLines()) {
+                        JSONObject receiptLineJson = new JSONObject();
+                        receiptLineJson.put("prodName", receiptLine.getProdName());
+                        receiptLineJson.put("price", receiptLine.getPrice());
+                        receiptLineJson.put("amount", receiptLine.getAmount());
+                        linesJson[i] = receiptLineJson;
                         i++;
                     }
-                    JSONObject receiptJson= new JSONObject();
+                    JSONObject receiptJson = new JSONObject();
                     receiptJson.put("storeName", API.getStoreName(receipt.getStoreId()));
                     receiptJson.put("userName", receipt.getUserName());
-
-                    receiptJson.put("totalCost",receipt.getTotalCost());
-                    receiptJson.put("lines",linesJson);
-                    receiptsJson[j]=receiptJson;
+                    receiptJson.put("receiptId",receipt.getReceiptId());
+                    receiptJson.put("totalCost", receipt.getTotalCost());
+                    receiptJson.put("lines", linesJson);
+                    receiptsJson[j] = receiptJson;
                     j++;
                 }
-                jsonOut.put("result",true);
-                jsonOut.put("data",receiptsJson);
+                jsonOut.put("result", true);
+                jsonOut.put("data", receiptsJson);
                 session.getRemote().sendString(jsonOut.toString());
 
-            }else{
-                jsonOut.put("result",false);
-                jsonOut.put("message",result.getData());
+            } else {
+                jsonOut.put("result", false);
+                jsonOut.put("message", result.getData());
                 session.getRemote().sendString(jsonOut.toString());
 
             }
+        } else if (type.equals("CANCEL_PURCHASE")) {
+
+            int receiptId =jo.getInt("receiptId");
+//            Result result= API.cancelPurchase(receiptId);//TODO merge with dorin
+            JSONObject jsonOut = new JSONObject();
+            jsonOut.put("type", "CANCEL_PURCHASE");
+//            jsonOut.put("result",result.isResult());
+//            jsonOut.put("message",result.getData());
+            session.getRemote().sendString(jsonOut.toString());
+
+
         }
     }
-
 }
 
